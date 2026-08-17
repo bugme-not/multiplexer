3ef605d63910c3a2d8a432ccc57eb631b0c5c9b2
bugme-not <d4em0n35@gmail.com>
2026-08-17 12:27:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-
+echo '#!/bin/sh
 if [ -n "$SSH_PASSWORD" ]; then
     echo "cxlvin:$SSH_PASSWORD" | chpasswd
-    echo "SSH password configured from environment variable"
 fi
-exec "$@"
+exec "$@"' > entrypoint.sh
